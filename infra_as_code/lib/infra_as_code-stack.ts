d636b42731747b8fdd596d301b1f128f604516ed
Sai Mahadev <mahadevvone@gmail.com>
2023-04-18 22:15:53 +0530

    code clean up

--- infra_as_code/lib/infra_as_code-stack.ts
+++ infra_as_code/lib/infra_as_code-stack.ts
@@ -3,18 +3,10 @@ import { Construct } from 'constructs';
 import { ApiGateway } from './ApiGateway';
 import { TestSyncLambda, test_sync_lambda_integration_options, test_sync_lambda_method_options } from './Lambdas/test_sync_lambda';
 import { TestAsyncLambda, test_async_lambda_integration_options, test_async_lambda_method_options } from './Lambdas/test_async_lambda';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class InfraAsCodeStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'InfraAsCodeQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
 
     const apigw = new ApiGateway(this);
 
